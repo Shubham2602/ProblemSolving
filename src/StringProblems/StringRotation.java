bb79679f598b00020a85fc02f971c5ede637d529
Shubham2602 <hellboyinvestorshubham@gmail.com>
2020-03-23 21:47:25 +0530

    first change

--- src/StringProblems/StringRotation.java
+++ src/StringProblems/StringRotation.java
@@ -7,21 +7,21 @@ public class StringRotation
 		return input.length()==toCheck.length() && (input+input).contains(toCheck);
 	}
 	
-	public String leftRotate(String input,int offset)
-	{
-		return (input.substring(offset,input.length()) + input.substring(0,offset));
-	}
-	
 	public String rightRotate(String input,int offset)
 	{
-		return (input.substring(input.length()-offset,input.length()) + (input.substring(0,(input.length()-offset))));
+		return (input.substring(input.length()-offset,input.length()) + input.substring(0,input.length()-offset));
+	}
+	
+	public String leftRotate(String input,int offset)
+	{
+		return (input.substring(input.length()-offset,input.length()) + (input.substring(0,(input.length()-offset))));	
 	}
 	
 	public static void main(String[] args) {
 		StringRotation sr = new StringRotation();
 
 		String input = "qwertyu";
-		String toCheck = "yuqwert";
+		String toCheck = "yqwert";
 		int offset = 2;
 		String str = "";
 		str = sr.isARotationOf(input, toCheck) ? "Is a Rotation" : "Is not a Rotation";
